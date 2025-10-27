2eb77dffc7e1636d5b80b871c28b73c18312232e
ericwu919 <157856302+ericwu919@users.noreply.github.com>
2025-10-27 03:23:01 -0400

    Refactor FIFO logic and improve readability

--- Part2/src/fifo_out.sv
+++ Part2/src/fifo_out.sv
@@ -19,77 +19,94 @@ module fifo_out #(
 		output logic [OUTW-1:0] OUT_AXIS_TDATA,
 		output logic OUT_AXIS_TVALID,
 		input OUT_AXIS_TREADY
-	);											 
+	);
 	
-	//Internal signals
-	integer unsigned [LOGDEPTH-1:0] capacity;	//for keeping track of the number of entries in the FIFO
-	logic [LOGDEPTH-1:0] write_address, read_address;	//read & write addresses to be sent to the memory  
+	// Internal registers
+	logic [$clog2(DEPTH+1)-1:0] capacity;	//for keeping track of the number of entries in the FIFO
+	logic [LOGDEPTH-1:0] write_address, read_address;	//read & write addresses to be sent to the memory
+	logic [LOGDEPTH-1:0] tail_next;
 	logic write_enable = IN_AXIS_TVALID && IN_AXIS_TREADY;	//write enable signal for the memory, head logic, and capacity logic
 	logic read_enable = OUT_AXIS_TVALID && OUT_AXIS_TREADY;	//read enable signal tail logic and capacity logic
-	
+
 	//Instantiation of the FIFO's dual-port memory
-	memory_dual_port #(OUTW, DEPTH) mem_inst(.clk(clk), .data_in(IN_AXIS_TDATA), .wr_en(write_enable), .write_addr(write_address), .read_addr(read_address), .data_out(OUT_AXIS_TDATA));	
+	memory_dual_port #(OUTW, DEPTH) mem_inst(.clk(clk), .data_in(IN_AXIS_TDATA), .wr_en(write_enable), .write_addr(write_address), .read_addr(tail_next), .data_out(OUT_AXIS_TDATA));	
 	
-	//Head (Write Address) Logic
+	// --------------------------------------------
+	// Head logic for FIFO
+	// --------------------------------------------
+
+	//Head (Write Address) Logic (occurs on every clock edge)
 	always_ff @(posedge clk) begin
-		
-		if (reset == 1)
-			//reset write address to 0
-			write_address <= 0;	
-			
-		else if (write_enable == 1)
-			//increment write address
-			write_address <= write_address + 1;  
-			
-		end	
-		
-	end
-	
-	//Tail (Read Address) Logic
-	always_ff @(posedge clk) begin
-		
 		if (reset == 1) begin
-			//reset output FIFO data to all 0s
-			OUT_AXIS_TDATA <= {OUTW{1'b0}};	//OUTW bits wide	
-			
-		end else begin	//unasserted reset
-			
-			if (capacity == DEPTH) begin	//FIFO is empty; no data to output
-				OUT_AXIS_TVALID <= 0;
-			
-			end else begin	//FIFO has data to be read
-				OUT_AXIS_TVALID <= 1;
-				
-				if (OUT_AXIS_TREADY == 1) begin	 
-					read_address <= ;
-				end	
-				
-			end			 
+			//initialize write address to 0
+			write_address <= 0;
+		end else if (write_enable == 1) begin
+			//increment write address
+			write_address <= write_address + 1; 
+		end
+	end
+
+	// TREADY signal combinational logic for head logic
+	always_comb begin
+		if (capacity == 0)	//FIFO is full; no space for new data
+			IN_AXIS_TREADY = 0;
+		else	//FIFO has space for new data to be written
+			IN_AXIS_TREADY = 1;
+	end			 
+	
+	// --------------------------------------------
+	// Tail logic for FIFO
+	// --------------------------------------------
+	
+	//Tail (Read Address) Logic (occurs on every clock edge)
+	always_ff @(posedge clk) begin
+		if (reset == 1) begin
+			read_address <= 0;
+
+		end else if (read_enable) begin	// reset == 0 && read_enable == 1
+			if (read_address == DEPTH-1)
+				read_address <= 0;
+			else
+				read_address <= read_address + 1;
 			
 		end	
 		
 	end	
 	
+	// For tail logic
+	always_comb begin
+		if (!read_enable)
+			tail_next = read_address;
+
+		else if (read_address == DEPTH-1)
+			tail_next = 0;
+
+		else
+			tail_next = read_address + 1;
+			
+		/*
+		if (capacity == DEPTH)	//FIFO is empty and has no data to be read
+			OUT_AXIS_TVALID = 0;
+
+		end else begin	// FIFO is not full; assert OUT_AXIS_TVALID; FIFO has data to be read
+			OUT_AXIS_TVALID = 1;
+
+		end
+		*/
+	end	
+
 	//Capacity Logic
 	always_ff @(posedge clk) begin
-		
 		if (reset == 1) begin
 			//empty the FIFO
-			capacity <= DEPTH;	
-			
+			capacity <= DEPTH;
 		end else begin	//unasserted reset
 			if (write_enable == 1)
 				//write is enabled; decrement capacity
 				capacity = capacity - 1;
-				
 			else if (read_enable == 1)
 				//read is enabled; increment capacity
 				capacity = capacity + 1;
-				
 			end	
-			
-		end	
-		
 	end
-	
 endmodule	
