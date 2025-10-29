041fc6afa06eacfae5eec3e86eecefe8aed7505e
Cody Chen <115575438+amazecow@users.noreply.github.com>
2025-10-29 17:38:42 -0400

    Refactor FIFO logic and improve readability
    When testing through simulation, there are no errors returned by the testbench.

--- Part2/src/fifo_out.sv
+++ Part2/src/fifo_out.sv
@@ -21,92 +21,86 @@ module fifo_out #(
 		input OUT_AXIS_TREADY
 	);
 	
-	// Internal registers
-	logic [$clog2(DEPTH+1)-1:0] capacity;	//for keeping track of the number of entries in the FIFO
+	// Internal signals
+	logic [DEPTH:0] capacity;	//for keeping track of the number of entries in the FIFO
 	logic [LOGDEPTH-1:0] write_address, read_address;	//read & write addresses to be sent to the memory
 	logic [LOGDEPTH-1:0] tail_next;
-	logic write_enable = IN_AXIS_TVALID && IN_AXIS_TREADY;	//write enable signal for the memory, head logic, and capacity logic
-	logic read_enable = OUT_AXIS_TVALID && OUT_AXIS_TREADY;	//read enable signal tail logic and capacity logic
+	logic write_enable, read_enable;
+	logic empty, full;
+
+	assign write_enable = IN_AXIS_TVALID && IN_AXIS_TREADY;	//write enable signal for the memory, head logic, and capacity logic
+	assign read_enable = OUT_AXIS_TVALID && OUT_AXIS_TREADY;	//read enable signal tail logic and capacity logic
+
+	assign empty = (capacity == DEPTH);
+	assign full = (capacity == 0);
+
+	assign OUT_AXIS_TVALID = !empty; // If FIFO is not empty, then OUT_AXIS_TVALID = 1
+	assign IN_AXIS_TREADY = (capacity > 0) || ((capacity == 0) && read_enable);
+	//assign IN_AXIS_TREADY = !full || (full && read_enable); // If FIFO is not full, then IN_AXIS_TREADY = 1
 
 	//Instantiation of the FIFO's dual-port memory
-	memory_dual_port #(OUTW, DEPTH) mem_inst(.clk(clk), .data_in(IN_AXIS_TDATA), .wr_en(write_enable), .write_addr(write_address), .read_addr(tail_next), .data_out(OUT_AXIS_TDATA));	
-	
+	memory_dual_port #(OUTW, DEPTH) mem_inst(
+		.clk(clk), .data_in(IN_AXIS_TDATA), .wr_en(write_enable), 
+		.write_addr(write_address), .read_addr(tail_next), .data_out(OUT_AXIS_TDATA));	
+
 	// --------------------------------------------
 	// Head logic for FIFO
 	// --------------------------------------------
 
 	//Head (Write Address) Logic (occurs on every clock edge)
 	always_ff @(posedge clk) begin
-		if (reset == 1) begin
+		if (reset) 
 			//initialize write address to 0
 			write_address <= 0;
-		end else if (write_enable == 1) begin
+		else if (write_enable) 
 			//increment write address
-			write_address <= write_address + 1; 
-		end
-	end
-
-	// TREADY signal combinational logic for head logic
-	always_comb begin
-		if (capacity == 0)	//FIFO is full; no space for new data
-			IN_AXIS_TREADY = 0;
-		else	//FIFO has space for new data to be written
-			IN_AXIS_TREADY = 1;
-	end			 
+			if (write_address == DEPTH - 1)
+				write_address <= 0;
+			else
+				write_address <= write_address + 1; 
+	end		 
 	
 	// --------------------------------------------
 	// Tail logic for FIFO
 	// --------------------------------------------
-	
+
 	//Tail (Read Address) Logic (occurs on every clock edge)
 	always_ff @(posedge clk) begin
-		if (reset == 1) begin
+		if (reset) 
 			read_address <= 0;
-
-		end else if (read_enable) begin	// reset == 0 && read_enable == 1
+		else if (read_enable) 	// reset == 0 && read_enable == 1
 			if (read_address == DEPTH-1)
 				read_address <= 0;
 			else
 				read_address <= read_address + 1;
-			
-		end	
-		
 	end	
 	
 	// For tail logic
 	always_comb begin
-		if (!read_enable)
-			tail_next = read_address;
-
-		else if (read_address == DEPTH-1)
-			tail_next = 0;
-
+		if (read_enable)
+			if (read_address == DEPTH-1)
+				tail_next = 0;
+			else
+				tail_next = read_address + 1;
 		else
-			tail_next = read_address + 1;
-			
-		/*
-		if (capacity == DEPTH)	//FIFO is empty and has no data to be read
-			OUT_AXIS_TVALID = 0;
-
-		end else begin	// FIFO is not full; assert OUT_AXIS_TVALID; FIFO has data to be read
-			OUT_AXIS_TVALID = 1;
-
-		end
-		*/
+			tail_next = read_address;
 	end	
 
-	//Capacity Logic
+	// --------------------------------------------
+	// Capacity Logic
+	// --------------------------------------------
+
 	always_ff @(posedge clk) begin
-		if (reset == 1) begin
+		if (reset) 
 			//empty the FIFO
 			capacity <= DEPTH;
-		end else begin	//unasserted reset
-			if (write_enable == 1)
-				//write is enabled; decrement capacity
-				capacity = capacity - 1;
-			else if (read_enable == 1)
-				//read is enabled; increment capacity
-				capacity = capacity + 1;
-			end	
+		 else 	//unasserted reset
+		 	case ({write_enable, read_enable})
+				2'b00: capacity <= capacity;
+				2'b01: capacity <= capacity + 1;
+				2'b10: capacity <= capacity - 1;
+				2'b11: capacity <= capacity;
+				default: capacity <= capacity;
+			endcase
 	end
 endmodule	
